2a9c93dd10f72d00fddd7bb3a038c59e051a82e5
Asher Abrams <asher813@aol.com>
2019-01-27 07:47:09 -0800

    Randomize addition problem.

--- MathQuiz02/Form1.cs
+++ MathQuiz02/Form1.cs
@@ -12,6 +12,24 @@ namespace MathQuiz02
 {
     public partial class Form1 : Form
     {
+        // Adding Random object called randomizer.
+        Random randomizer = new Random();
+        // Creating computer-generated addends for addition problem.
+        int addend1;
+        int addend2;
+
+        /// <summary>
+        /// Start the quiz - create problems, start timer.
+        /// </summary>
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            plusLeftLabel = addend1.ToString();
+            plusRightLabel = addend2.ToString();
+            sum.Value = 0;
+        }
+
         public Form1()
         {
             InitializeComponent();
